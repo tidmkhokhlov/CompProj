632dfa1508be4ef25961cf06b9c9b2a13a2edea9
Andrey Ikonnikov <andreyiko@inbox.ru>
2024-10-08 00:55:27 +0300

    colors

--- comp.sh
+++ comp.sh
@@ -11,12 +11,12 @@ rm env.img
 mkdir back
 
 #reading X
-echo "Enter X ammount."
+echo -e "\e[4mEnter X ammount.\e[0m"
 read X
-D=$X * 0.7
+D= $X * 0.7
 
 #reading log
-Y=sudo du -s log |awk '{print $1}'
+Y= du -s log | awk '{print $1}'
 # echo $Y
 
 #main condition
@@ -24,15 +24,16 @@ if [$Y -le $D]
 then
 	#archiving
 	sudo tar -czf back.tar.gz log
-	echo "/log archived"
+	echo -e "\e[42mLog Archived\e[0m"
 	#removing files in log
 	sudo rm -rf log/*
 	#backup moving
 	mv back.tar.gz back
-	echo "archive moved"
+	echo -e "\e[42mArchive moved\e[0m"
 fi
 
 #unmounting limited folder
 sudo umount log
+echo -e "Log folder \e[4munmounted\e[0m."
 
 
