fcff9eb665c538ac825d6309394a9f4105defab6
Andrey Ikonnikov <andreyiko@inbox.ru>
2024-10-07 22:44:40 +0300

    virtual folder created

--- comp.sh
+++ comp.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
 
-#set up
+#env
 mkdir log
-mkdir backup
+dd if=/dev/zero of=env.img bs=1M count=100
+mkfs.ext4 env.img
+sudo mount -o loop env.img log
+rm env.img
+mkdir back
 
 #reading X
-X=shell
 echo "Enter X ammount."
 read X
-D=X * 0.7
+D=$X * 0.7
 
 #reading log
-Y= du -s log |awk '{print $1}'
+Y=sudo du -s log |awk '{print $1}'
 echo $Y
 
 #main condition
 if [$Y -le $D]
 then
 	#archiving
-	tar -czf back.tar.gz log
+	sudo tar -czf back.tar.gz log
 	echo "/log archived"
 	#removing files in log
-	rm -r log/*
+	sudo rm -r log/*
 	#backup moving
-	mv back.tar.gz backup
+	mv back.tar.gz back
 	echo "archive moved"
 fi
 
+#unmounting limited folder
+sudo umount log
 
 
