55ed61bf48bf327e9592426ee68bfa6e9e41a044
tidmkhokhlov <tidmkhokhlov@edu.hse.ru>
2024-10-09 16:17:25 +0300

    update archiving

--- comp.sh
+++ comp.sh
@@ -26,33 +26,28 @@ fi
 CURRENT_SIZE=$(du -sb "$LOG_DIR" | awk '{print $1}')
 
 #main condition
-if [ "$CURRENT_SIZE" -ge "$THRESHOLD_SIZE" ];
-then
-	#archiving
-	echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Начинаю архивирование..."
+if [ "$CURRENT_SIZE" -ge "$THRESHOLD_SIZE" ]; then
+    echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Начинаю архивирование..."
 
-	N=5
+    # Находим N старейших файлов
+    N=5
     OLD_FILES=$(ls -t "$LOG_DIR" | tail -n "$N")
-
-	if [ -z "$OLD_FILES" ]; then
+    if [ -z "$OLD_FILES" ]; then
         echo "Нет файлов для архивирования."
         exit 0
     fi
 
-	TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+    # Создание архива
+    TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
     ARCHIVE_FILE="$BACKUP_DIR/archive_$TIMESTAMP.tar.gz"
-
     tar -czf "$ARCHIVE_FILE" -C "$LOG_DIR" $OLD_FILES
 
-	echo -e "\e[42mLog Archived\e[0m"
-	#removing files in "$LOG_DIR"
-	for file in $OLD_FILES; do
+    # Удаление архивированных файлов
+    for file in $OLD_FILES; do
         rm "$LOG_DIR/$file"
     done
 
     echo "Файлы архивированы в $ARCHIVE_FILE и удалены из $LOG_DIR."
-
-	echo -e "\e[42mArchive moved\e[0m"
 else
     echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Архивирование не требуется."
 fi
