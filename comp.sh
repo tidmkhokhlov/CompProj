86ca926fc0323dd5ea09f3776f5c51f3ab7ee060
tidmkhokhlov <tidmkhokhlov@edu.hse.ru>
2024-10-09 16:39:17 +0300

    moving env from comp.sh to test.sh

--- comp.sh
+++ comp.sh
@@ -6,21 +6,7 @@ BACKUP_DIR="$SCRIPT_DIR/backup"
 MAX_SIZE=$((2 * 1024 * 1024 * 1024))  # 2 ГБ в байтах
 THRESHOLD_SIZE=$((MAX_SIZE * 70 / 100))  # 70% от 2 ГБ
 
-#env
-if [ ! -d "$LOG_DIR" ]; 
-then
-  mkdir "$LOG_DIR"
-fi
-dd if=/dev/zero of=env.img bs=1M count=100
-mkfs.ext4 env.img
-sudo mount -o loop env.img "$LOG_DIR"
-sudo chmod 777 "$LOG_DIR"
-rm -rf "$LOG_DIR"/lost+found
-rm env.img
-if [ ! -d "$BACKUP_DIR" ]; 
-then
-  mkdir "$BACKUP_DIR"
-fi
+mkdir -p "$LOG_DIR" "$BACKUP_DIR"
 
 #reading log
 CURRENT_SIZE=$(du -sb "$LOG_DIR" | awk '{print $1}')
@@ -52,8 +38,6 @@ else
     echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Архивирование не требуется."
 fi
 
-#unmounting limited folder
-sudo umount "$LOG_DIR"
-echo -e "$LOG_DIR folder \e[4munmounted\e[0m."
+
 
 
--- test.sh
+++ test.sh
@@ -6,6 +6,14 @@ BACKUP_DIR="$SCRIPT_DIR/backup"
 
 mkdir -p "$LOG_DIR" "$BACKUP_DIR"
 
+#env
+dd if=/dev/zero of=env.img bs=1M count=100
+mkfs.ext4 env.img
+sudo mount -o loop env.img "$LOG_DIR"
+sudo chmod 777 "$LOG_DIR"
+rm -rf "$LOG_DIR"/lost+found
+rm env.img
+
 rm -rf "$LOG_DIR"/* "$BACKUP_DIR"/*
 
 for i in {1..10}; do
@@ -14,3 +22,8 @@ done
 
 # Тест 1: Проверка архивирования при заполниность > 70%
 echo "Запуск теста 1..."
+
+
+#unmounting limited folder
+sudo umount "$LOG_DIR"
+echo -e "$LOG_DIR folder \e[4munmounted\e[0m."
