ab6c7836cc88cbe2da49c95aa094835f9563957d
tidmkhokhlov <tidmkhokhlov@edu.hse.ru>
2024-10-09 16:12:46 +0300

    fixed archiving

--- comp.sh
+++ comp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(dirname "$0")" # Получаем директорию скрипта
 LOG_DIR="$SCRIPT_DIR/log"
 BACKUP_DIR="$SCRIPT_DIR/backup"
 MAX_SIZE=$((2 * 1024 * 1024 * 1024))  # 2 ГБ в байтах
@@ -29,13 +29,32 @@ CURRENT_SIZE=$(du -sb "$LOG_DIR" | awk '{print $1}')
 if [ "$CURRENT_SIZE" -ge "$THRESHOLD_SIZE" ];
 then
 	#archiving
-	find "$LOG_DIR" -type f -printf '%T+ %p\n' | sort | head -n $CURRENT_SIZE-$D | cut -d' ' -f2- | tar -czf "$BACKUP_DIR"$(date +'%CURRENT_SIZE-%m-%d_%H-%M-%S').tar.gz -T -
+	echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Начинаю архивирование..."
+
+	N=5
+    OLD_FILES=$(ls -t "$LOG_DIR" | tail -n "$N")
+
+	if [ -z "$OLD_FILES" ]; then
+        echo "Нет файлов для архивирования."
+        exit 0
+    fi
+
+	TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+    ARCHIVE_FILE="$BACKUP_DIR/archive_$TIMESTAMP.tar.gz"
+
+    tar -czf "$ARCHIVE_FILE" -C "$LOG_DIR" $OLD_FILES
+
 	echo -e "\e[42mLog Archived\e[0m"
 	#removing files in "$LOG_DIR"
-	sudo rm -rf "$LOG_DIR"/*
-	#backup moving
-	mv "$BACKUP_DIR"$(date +'%CURRENT_SIZE-%m-%d_%H-%M-%S').tar.gz "$BACKUP_DIR"
+	for file in $OLD_FILES; do
+        rm "$LOG_DIR/$file"
+    done
+
+    echo "Файлы архивированы в $ARCHIVE_FILE и удалены из $LOG_DIR."
+
 	echo -e "\e[42mArchive moved\e[0m"
+else
+    echo "Папка $LOG_DIR заполнена на $(($CURRENT_SIZE / 1024 / 1024)) MB. Архивирование не требуется."
 fi
 
 #unmounting limited folder
